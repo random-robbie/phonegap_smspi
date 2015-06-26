123dff1d87db5646a6d7789c762a9987bf4662b2
txt3rob <txt3rob@gmail.com>
2015-06-26 16:28:48 +0100

    Mobile app from post to request

--- js/index.js
+++ js/index.js
@@ -1,3 +1,47 @@
+document.addEventListener("deviceready", onDeviceReady, false);
+ 
+function onDeviceReady() {
+    // we will not be doing anything!!
+}
+ 
+$(document).on("pageshow", function () {
+    $.mobile.loading("hide");
+    $("body").removeClass('ui-disabled');
+    if ($("#contactsList").length == 1) {
+        $("body").addClass('ui-disabled').css("background", "#000");
+        $.mobile.loading("show");
+        var options = new ContactFindOptions();
+        options.filter = "";
+        options.multiple = true;
+        var filter = ["displayName", "phoneNumbers"];
+        navigator.contacts.find(filter, onSuccess, onError, options);
+    }
+});
+ 
+function onSuccess(contacts) {
+    var html = "";
+    for (var i = 0; i < contacts.length; i++) {
+        if ($.trim(contacts[i].displayName).length != 0 || $.trim(contacts[i].nickName).length != 0) {
+            if (contacts[i].phoneNumbers) {
+               
+				document.getElementById("to").setAttribute('value',contacts[i].phoneNumbers[j].value);
+        }
+    }
+    if (contacts.length === 0) {
+        html = '<li data-role="collapsible" data-iconpos="right" data-shadow="false" data-corners="false">';
+        html += '<h2>No Contacts</h2>';
+        html += '<label>No Contacts Listed</label>';
+        html += '</li>';
+    }
+}
+ 
+function onError(contactError) {
+    alert('Oops Something went wrong!');
+    $.mobile.loading("hide");
+    $("body").removeClass('ui-disabled');
+}
+
+
 function setCookie(cname,cvalue,exdays) {
     var d = new Date();
     d.setTime(d.getTime() + (exdays*24*60*60*1000));
